35ea1b9d1c3aea08787314c4aca01133f2983836
Andrey Vinkovatov <a.vinkovatov@smartcat.ai>
2020-05-03 15:41:52 +0300

    main page floor1 - 1,5h

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="wrapper">
-
+    <div class="floor1">
+      <div class="left">
+        <h2 class="h2">Компания BMV Engineering</h2>
+        <h1 class="h1">Более 20 лет занимаемся вентиляцией, автоматикой и кондиционированием</h1>
+        <a href="/" @click.prevent="$store.commit('openModal', 'contactUs')" class="big button">Бесплатная консультация</a>
+      </div>
+      <img src="/img/main-screen.jpg" alt="Главный экран" class="right">
+    </div>
   </div>
 </template>
 
@@ -11,7 +18,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.text {
-  max-width: 720px;
+.floor1 {
+  padding-top: 30px;
+  display: flex;
+  .left {
+    width: 50%;
+    padding-right: 5%;
+  }
+  .right {
+    width: 50%;
+  }
 }
 </style>
